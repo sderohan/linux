27f7a6391cfd1dc043c7340ca33e1dda0f18673c
sderohan <rohanshinde7212@gmail.com>
2021-09-17 23:21:59 +0530

    add check if given number is an armstrong number

---
+++ bash-scripts/basics/check_armstrong_number.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+declare -i num
+read -p "Enter the number " num
+
+declare -i result copy_num=$num
+
+while [ $copy_num -gt 0 ]
+do
+  cur_num=$(($copy_num%10))
+  result=$((result + cur_num*cur_num*cur_num))
+  copy_num=$((copy_num/10))
+done
+
+if [ $result -eq $num ]
+then
+  echo "$num is an armstrong number"
+else
+  echo "$num is not an armstrong number"
+fi
