7a8f1725a1091dd6952f2a35422e7054082b5faf
sderohan <rohanshinde7212@gmail.com>
2021-09-29 23:38:44 +0530

    add multi-line comment in sleepIn

--- warmup/sleepIn.sh
+++ warmup/sleepIn.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-# The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. 
-# We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+
+: <<'END_COMMENT'
+ The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. 
+ We sleep in if it is not a weekday or we're on vacation. 
+ Return true if we sleep in. 
+END_COMMENT
 
 function sleepIn() {
-	return $(( $1 == 0 || $2 == 1))
+	return $(( $1 == 0 || $2 == 1 ))
 }
 
 function main() {
