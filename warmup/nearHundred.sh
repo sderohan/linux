56c90043a65dcf797f481a21a35232e157ac51f6
sderohan <rohanshinde7212@gmail.com>
2021-10-02 12:39:04 +0530

    update logic directory name

---
+++ warmup/nearHundred.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Given an int n, return true if it is within 10 of 100 or 200. 
+
+function nearHundred() {
+	
+}
+
+function main() {
+	nearHundred 93
+	echo $?
+	nearHundred 90
+	echo $?
+	nearHundred 89
+	echo $?
+}
+
+main
