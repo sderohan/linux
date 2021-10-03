746a0eb06d5f3ac856365bd5e714a451fca76ce2
sderohan <rohanshinde7212@gmail.com>
2021-10-03 23:32:25 +0530

    add soultion for the nearHundred problem

--- warmup/nearHundred.sh
+++ warmup/nearHundred.sh
@@ -3,7 +3,24 @@
 # Given an int n, return true if it is within 10 of 100 or 200. 
 
 function nearHundred() {
-	
+	abs $1
+	(( diff1=$1-100))
+	(( diff2=$1-200))
+	abs $diff1
+	diff1=$?
+	abs $diff2
+	diff2=$?
+	return $(( diff1 <= 10 || diff2 <= 10))		
+}
+
+# Helper function to calculate the absolute value
+function abs() {
+	declare -i n=$1
+	if (( $1 < 0 ))
+	then
+		return $(( $1 * -1))
+	fi
+	return $1
 }
 
 function main() {
@@ -12,6 +29,14 @@ function main() {
 	nearHundred 90
 	echo $?
 	nearHundred 89
+	echo $?
+	nearHundred 110
+	echo $?
+	nearHundred -101
+	echo $?
+	nearHundred -209
+	echo $?
+	nearHundred -50
 	echo $?
 }
 
