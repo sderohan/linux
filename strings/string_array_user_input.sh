a1c8066fa41a5a4584ecc6c37feca4c9d357836d
sderohan <rohanshinde7212@gmail.com>
2021-10-26 23:56:42 +0530

    update program by adding comments

--- strings/string_array_user_input.sh
+++ strings/string_array_user_input.sh
@@ -8,6 +8,8 @@ read -p 'Enter the number of words to form string array ' n
 for (( i=1; i <= n; i++))
 do
   read -p "Enter $i the word " word
+  # There are different ways to perform the append operation to array in bash,
+  # If you want to know more, just google it
   arr[${#arr[@]}]=$word
 done
 
