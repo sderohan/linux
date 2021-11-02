1d09c9e48914545018653165050994e8cef1cbdc
sderohan <rohanshinde7212@gmail.com>
2021-11-02 23:08:49 +0530

    Update to read file using other approaches

--- file-handling/different_ways_to_read_file.sh
+++ file-handling/different_ways_to_read_file.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+:<<'END'
+This file contains the different ways to read the file in the bash script
+END
+
 echo "Reading file using cat command"
 file_content=`cat notes.txt`
 echo $file_content
 
+echo -e "\nReading file using \$(<filename)"
+file_content=$(<notes.txt)
+echo $file_content
+
+echo -e "\nReading file content using script"
+file_name='notes.txt'
+while read line   # to read the file omitting the backslash use [ while read -r line ]
+do
+	echo $line
+done < $file_name # here we can write the filename like [ done <notes.txt ]
